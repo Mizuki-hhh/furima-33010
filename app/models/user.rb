6e93e4908b42b3950fc1dd533ab47d79fe72d549
Mizuki-hhh <higashishita.m@gmail.com>
2020-12-20 12:20:02 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
     validates :birthday, presence: true
     validates :email, uniqueness: true
     validates :password_confirmation, presence: true
-    PASSWORD_REGEX = /\A[a-zA-Z0-9]+\z/.freeze
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
     validates_format_of :password, :password_confirmation,  with: PASSWORD_REGEX 
     NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/.freeze
     validates_format_of :last_name, :first_name, with: NAME_REGEX
