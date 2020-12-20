0a89c5982c00b887e067b560698d8d3591c18f79
Mizuki-hhh <higashishita.m@gmail.com>
2020-12-20 23:20:03 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,25 @@
 class Item < ApplicationRecord
   belongs_to :user
-  
+
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping_charge
+  belongs_to_active_hash :shipping_date
+
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :price
+  end
+
+  validates :category_id,        numericality: { other_than: 1 }
+  validates :condition_id,       numericality: { other_than: 1 }
+  validates :prefecture_id,      numericality: { other_than: 1 }
+  validates :shipping_charge_id, numericality: { other_than: 1 }
+  validates :shipping_date_id,   numericality: { other_than: 1 }
+  validates :category_id,        numericality: { other_than: 1 }
 end
