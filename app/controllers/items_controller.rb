62b29f96c809e779263343214d601d5cdb8c7be4
Mizuki-hhh <higashishita.m@gmail.com>
2020-12-21 11:28:08 +0900

    商品出品ページを表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,5 +3,22 @@ class ItemsController < ApplicationController
     @items = Item.order("created_at DESC")
   end
 
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
   
+  private
+
+  def item_pramas
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :shipping_charge_id, :prefecture_id, :shipping_date_id, :price)
+  end
 end
