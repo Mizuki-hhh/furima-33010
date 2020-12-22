92301576aec7ab7ef7a83ae5a05b3674b1f49ce4
Mizuki-hhh <higashishita.m@gmail.com>
2020-12-22 15:45:04 +0900

    リンク設定とコントローラ編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @items = Item.all.order("created_at DESC")
@@ -37,7 +37,9 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
   end
 
   
