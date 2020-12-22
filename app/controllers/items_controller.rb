5c56e3f581015ca43f4fb31ba15fd3e69a4dd07f
Mizuki-hhh <higashishita.m@gmail.com>
2020-12-22 18:09:53 +0900

    renderの先をshowに戻すようにした

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -38,8 +38,11 @@ class ItemsController < ApplicationController
 
   def destroy
     if @item.user_id == current_user.id
-      @item.destroy
-      redirect_to root_path
+      if @item.destroy
+        redirect_to root_path
+      else
+        render :show
+      end
     end
   end
 
