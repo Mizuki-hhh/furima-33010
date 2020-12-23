40fc798481e2138d3a70829af67dd87eaf79d999
Mizuki-hhh <higashishita.m@gmail.com>
2020-12-23 19:07:25 +0900

    購入情報保存成功

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,13 +3,15 @@ class RecordsController < ApplicationController
   def index
     @purchase_record = PurchaseRecord.new
     @item = Item.find(params[:item_id])
+
+  
   end
 
   def create
     @purchase_record = PurchaseRecord.new(record_params)
     if @purchase_record.valid?
       @purchase_record.save
-      redirect_to action: root_path
+      redirect_to root_path
     else
       render action: :index
     end
@@ -17,7 +19,7 @@ class RecordsController < ApplicationController
 
   private
   def record_params
-    params.require(:purchase_record).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id)
+    params.require(:purchase_record).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :record_id).merge(user_id: current_user.id, item_id: params[:item_id] )
   end
 
 end
