4b765ca2ab1786be9165c7c1b3f47fcc17f3bfe1
Mizuki-hhh <higashishita.m@gmail.com>
2020-12-25 21:27:54 +0900

    Basic認証導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :configure_permitted_parameters, if: :devise_controller?
 
 
@@ -6,5 +7,11 @@ class ApplicationController < ActionController::Base
   
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :furigana_last_name, :furigana_first_name, :birthday])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["FURIMA_AUTH_USER"] && password == ENV["FURIMA_AUTH_PASSWORD"]
+    end
   end
 end
