ada0a3de44845ad6c7f76b39439c65286ae4c7c4
Mizuki-hhh <higashishita.m@gmail.com>
2020-12-30 15:54:17 +0900

    アソシエーションとviewファイル編集

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :record
 
-  has_one_attached :image
+  has_many_attached :images
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :category
