0f45054a9211570a9c474d9965e5c0db89a30994
Mizuki-hhh <higashishita.m@gmail.com>
2021-01-04 18:55:04 +0900

    ja.yml記述

--- app/models/purchase_record.rb
+++ app/models/purchase_record.rb
@@ -3,7 +3,7 @@ class PurchaseRecord
   attr_accessor :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id, :record_id, :token
 
   with_options presence: true do
-    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/}
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :city
     validates :house_number
